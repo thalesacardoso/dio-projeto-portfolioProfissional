9c31861c81e33621968ab1eee2513897e330691c
thalesacardoso <thales.dev@hotmail.com>
2023-09-27 14:26:34 -0300

    Criação da API + perfil completo

---
+++ assets/js/api.js
@@ -0,0 +1,11 @@
+
+async function apiGithub() {
+   let url = 'https://raw.githubusercontent.com/thalesacardoso/dio-projeto-portfolioProfissional/main/data/profile.json'
+
+   await fetch(url)
+   .then(response => response.json())
+   .then(dados => {
+      mudarPerfil(dados)
+      mudarSkill(dados)
+   })
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@ function abrirAba(nomeAba, i) {
    aba.classList.toggle('bgcolor')
    conteudo.classList.toggle('oculto')
 
-   if(aba.classList[0] == 'bgcolor'){
+   if (aba.classList[0] == 'bgcolor') {
       titulo.style.border = '0'
       seta.style.rotate = '180deg'
    } else {
@@ -18,3 +18,36 @@ function abrirAba(nomeAba, i) {
       seta.style.rotate = '0deg'
    }
 }
+
+
+function mudarPerfil(dados) {
+
+   let perfil = {}
+   perfil.foto = document.getElementsByClassName('api-perfil')[0]
+   perfil.foto.src = dados.photo
+   perfil.foto.alt = 'Foto do perfil'
+
+   perfil.nome = document.getElementsByClassName('api-perfil')[1]
+   perfil.nome.innerHTML = dados.name
+
+   perfil.job = document.getElementsByClassName('api-perfil')[2]
+   perfil.job.innerHTML = dados.job
+
+   perfil.endereco = document.getElementsByClassName('api-perfil')[3]
+   perfil.endereco.innerHTML = dados.location
+
+   perfil.telefone = document.getElementsByClassName('api-perfil')[4]
+   perfil.telefone.innerHTML = dados.phone
+
+   perfil.email = document.getElementsByClassName('api-perfil')[5]
+   perfil.email.innerHTML = dados.email
+
+}
+
+
+function mudarSkill(dados) {
+
+}
+
+
+apiGithub();
